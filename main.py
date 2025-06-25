e225ba1099e1314294c55af7046511c7459cb4e2
Donald Cameron <donald.cameron@qa.com>
2025-06-25 15:26:02 +0100

    Moved file loading into load_file() function

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import logging
 
-def main():
+def load_file():
     file_name = "C:\\tmp\\KeyboardHandler.txt"
+    lines = []
 
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
             for line in file:
-                # process the line
-                print(line.strip())
+                lines.append(line.strip())
     except IOError as ex:
         logging.error("File reading error: %s", ex)
+
+    return lines
+
+def main():
+    lines = load_file()
+
+    for line in lines:
+        print(line)
 
 
 if __name__ == "__main__":
