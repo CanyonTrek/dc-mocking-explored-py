0ffef0fbd4bcc77f89161587c5dcd6dac9c9c265
Donald Cameron <donald.cameron@qa.com>
2025-06-25 16:47:03 +0100

    Added lambda function to load data into collection

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from app.text_file_loader import TextFileLoader
 
 def main():
     tfl = TextFileLoader()
-    lines = tfl.load_file("C:/tmp/KeyboardHandler.java.txt")
 
-    for line in lines:
-        print(line)
+    # Define a lambda-like function to append the line to the list
+    line_collector = lambda collection, line: collection.append(line)
+
+    # Load the file with the processing function
+    lines = tfl.load_file("C:/tmp/KeyboardHandler.java.txt", line_collector)
+
+    # Print the lines with a prefix
+    for element in lines:
+        print(f">> {element}")
 
 if __name__ == "__main__":
     main()
