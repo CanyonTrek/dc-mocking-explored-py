b674f6e75f7ddafa10874eb769afee5e773b2b9c
Donald Cameron <donald.cameron@qa.com>
2025-06-26 11:10:17 +0100

    Added test_count_chars_in_data_source with mocking

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,6 +1,7 @@
 import unittest
 from app.text_file_source import TextFileSource
 from app.data_loader import DataLoader
+from unittest.mock import Mock
 
 class TestDataLoader(unittest.TestCase):
     def test_count_chars_in_data_source_no_mocking(self):
@@ -28,6 +29,22 @@ class TestDataLoader(unittest.TestCase):
 
         # assert
         self.assertEqual(expected, result)
+
+
+    def test_count_chars_in_data_source_with_mocking(self):
+        # arrange
+        mock_data_source = Mock()
+        mock_lines = ["line 1", "line 2", "line 3"]
+        mock_data_source.load_data.return_value = mock_lines
+
+        cut = DataLoader(mock_data_source)
+        expected = 18  # 6 + 6 + 6
+
+        # act
+        result = cut.load_data("")
+
+        # assert
+        self.assertEqual(result, expected)
 
 
 
